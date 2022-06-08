45f625a943dcb14a2276bc739f3859e8907a9184
Vl4dz85 <Vl4dz85@gmail.com>
2022-06-08 23:24:34 +0400

    custom iterator implementation

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-// const sequence = new Promise((res) => {
-//   let count = 1;
-//   setInterval(() => {
-//     res(count++);
-//   });
-// });
+class CustomIterator {
+  private cursor = 0;
+  private value: number;
 
-// sequence.then((v) => console.log(v));
+  constructor(private arr: number[], private divisor: number = 1) {}
+  next() {
+    while (this.cursor < this.arr.length) {
+      this.value = this.arr[this.cursor++];
+      if (this.value % this.divisor === 0) {
+        return { done: false, value: this.value };
+      }
+    }
+    return { done: true, value: this.value };
+  }
+  [Symbol.iterator]() {
+    return {
+      next: this.next.bind(this)
+    };
+  }
+}
 
-// const sequence = (function* iteratorFn() {
-//   let item = 1;
-//   while (item) {
-//     yield item++;
-//   }
-// })();
+const consumer = new CustomIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
 
-// console.log(sequence.next().value);
+for (let item of consumer) {
+  console.log("iteration 1 ", item);
+}
 
-// console.log(sequence.next().value);
-
-// console.log(sequence.next().value);
-
-import { interval } from "rxjs";
-
-interval(1000).subscribe((v: number) => {
-  console.log(v);
-});
+Array.from(consumer).forEach((v) => console.log("iteration 2 " + v));
