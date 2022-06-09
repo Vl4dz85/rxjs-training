746116a3103e9b385d55cf7014108a3e52096fb0
Vl4dz85 <Vl4dz85@gmail.com>
2022-06-09 14:46:02 +0400

    producer implementation

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,73 @@
-class CustomIterator {
-  private cursor = 0;
-  private value: number;
+// class CustomIterator {
+//   private cursor = 0;
+//   private value: number;
 
-  constructor(private arr: number[], private divisor: number = 1) {}
-  next() {
-    while (this.cursor < this.arr.length) {
-      this.value = this.arr[this.cursor++];
-      if (this.value % this.divisor === 0) {
-        return { done: false, value: this.value };
-      }
-    }
-    return { done: true, value: this.value };
-  }
-  [Symbol.iterator]() {
+//   constructor(private arr: number[], private divisor: number = 1) {}
+//   next() {
+//     while (this.cursor < this.arr.length) {
+//       this.value = this.arr[this.cursor++];
+//       if (this.value % this.divisor === 0) {
+//         return { done: false, value: this.value };
+//       }
+//     }
+//     return { done: true, value: this.value };
+//   }
+//   [Symbol.iterator]() {
+//     return {
+//       next: this.next.bind(this)
+//     };
+//   }
+// }
+
+// const consumer = new CustomIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+
+// for (let item of consumer) {
+//   console.log("iteration 1 ", item);
+// }
+
+// Array.from(consumer).forEach((v) => console.log("iteration 2 " + v));
+interface IListener {
+  next(message: string): void;
+}
+
+class Producer {
+  private listeners: IListener[] = [];
+  public subscribe(listener: IListener) {
+    const index = this.listeners.push(listener);
     return {
-      next: this.next.bind(this)
+      unsubscribe: () => {
+        this.listeners.slice(index - 1, 1);
+      }
     };
   }
+
+  public notify(message: string) {
+    this.listeners.forEach((listener: IListener) => {
+      listener.next(message);
+    });
+  }
 }
 
-const consumer = new CustomIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+const listener1 = {
+  next(message: string) {
+    console.log("listener 1", message);
+  }
+};
 
-for (let item of consumer) {
-  console.log("iteration 1 ", item);
-}
+const notifier = new Producer();
 
-Array.from(consumer).forEach((v) => console.log("iteration 2 " + v));
+const listener2 = {
+  next(message: string) {
+    console.log("listener 2", message);
+  }
+};
+
+const sub1 = notifier.subscribe(listener1);
+const sub2 = notifier.subscribe(listener2);
+
+notifier.notify("Hi all RxJS is awesome");
+sub1.unsubscribe();
+
+setTimeout(() => {
+  notifier.notify("After unsubscribe ");
+}, 5000);
