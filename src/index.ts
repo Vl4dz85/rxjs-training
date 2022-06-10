bc7716749c9b5a40d00dda3aafbb19515cfbf863
Vl4dz85 <Vl4dz85@gmail.com>
2022-06-10 15:17:14 +0400

    cold sequence example

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
-import { Observable } from "rxjs";
+import { interval } from "rxjs";
 
-const sequence$ = new Observable((subscriber) => {
-  let count = 1;
-  const intervalId = setInterval(() => {
-    subscriber.next(count++);
-    if (count % 5 === 0) {
-      clearInterval(intervalId);
-      subscriber.complete();
-      return;
-    }
-  }, 1000);
-  return () => {
-    console.log("unsubscribe");
-    clearInterval(intervalId);
-  };
+const sequence$ = interval(1000);
+
+const sub1 = sequence$.subscribe((v) => {
+  console.log("Sub 1", v);
 });
 
-const subscription = sequence$.subscribe(
-  (v) => {
-    console.log(v);
-  },
-  () => {},
-  () => console.log("completed")
-);
-
-setInterval(() => {
-  subscription.unsubscribe();
-}, 3000);
+setTimeout(() => {
+  sequence$.subscribe((v) => {
+    console.log("Sub 2", v);
+  });
+}, 5000);
